47a0cf6be45ff18b7cf98d15e4a6f199956959d6
hanifan <hanifannurulhaq@outlook.com>
2023-10-01 20:27:40 +0700

    update app configuration

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:piduiteun/counter/counter.dart';
 import 'package:piduiteun/l10n/l10n.dart';
+import 'package:piduiteun/routes/route.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
         useMaterial3: true,
+        colorSchemeSeed: const Color.fromRGBO(0, 103, 127, 1),
+        fontFamily: 'Nunito',
+        // colorScheme: const ColorScheme(
+        //   background: Color.fromRGBO(251, 252, 254, 1), 
+        //   onBackground: Color.fromRGBO(25, 28, 29, 1),
+        //   brightness: Brightness.light, 
+        //   primary: Color.fromRGBO(0, 103, 127, 1), 
+        //   onPrimary: Colors.white, 
+        //   secondary: Color.fromRGBO(45, 107, 39, 1), 
+        //   onSecondary: Colors.white, 
+        //   error: Color.fromRGBO(186, 26, 26, 1), 
+        //   onError: Colors.white, 
+        //   surface: Color.fromRGBO(251, 252, 254, 1),
+        //   onSurface: Color.fromRGBO(25, 28, 29, 1),
+        // ),
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      routerConfig: router,
     );
   }
 }
