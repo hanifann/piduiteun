76b608ff32742d2696168bff50bb15c16afff034
hanifan <hanifannurulhaq@outlook.com>
2023-10-01 20:39:51 +0700

    initialized screen util

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:piduiteun/l10n/l10n.dart';
 import 'package:piduiteun/routes/route.dart';
 
@@ -7,28 +8,33 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: const Color.fromRGBO(0, 103, 127, 1),
-        fontFamily: 'Nunito',
-        // colorScheme: const ColorScheme(
-        //   background: Color.fromRGBO(251, 252, 254, 1), 
-        //   onBackground: Color.fromRGBO(25, 28, 29, 1),
-        //   brightness: Brightness.light, 
-        //   primary: Color.fromRGBO(0, 103, 127, 1), 
-        //   onPrimary: Colors.white, 
-        //   secondary: Color.fromRGBO(45, 107, 39, 1), 
-        //   onSecondary: Colors.white, 
-        //   error: Color.fromRGBO(186, 26, 26, 1), 
-        //   onError: Colors.white, 
-        //   surface: Color.fromRGBO(251, 252, 254, 1),
-        //   onSurface: Color.fromRGBO(25, 28, 29, 1),
-        // ),
-      ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      routerConfig: router,
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: (context, widget) {
+        return MaterialApp.router(
+          theme: ThemeData(
+            useMaterial3: true,
+            colorSchemeSeed: const Color.fromRGBO(0, 103, 127, 1),
+            fontFamily: 'Nunito',
+            // colorScheme: const ColorScheme(
+            //   background: Color.fromRGBO(251, 252, 254, 1), 
+            //   onBackground: Color.fromRGBO(25, 28, 29, 1),
+            //   brightness: Brightness.light, 
+            //   primary: Color.fromRGBO(0, 103, 127, 1), 
+            //   onPrimary: Colors.white, 
+            //   secondary: Color.fromRGBO(45, 107, 39, 1), 
+            //   onSecondary: Colors.white, 
+            //   error: Color.fromRGBO(186, 26, 26, 1), 
+            //   onError: Colors.white, 
+            //   surface: Color.fromRGBO(251, 252, 254, 1),
+            //   onSurface: Color.fromRGBO(25, 28, 29, 1),
+            // ),
+          ),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          routerConfig: router,
+        );
+      },
     );
   }
 }
