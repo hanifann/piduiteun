96b2db55adf463119330fb16744575c6f2ecfbda
hanifan <hanifannurulhaq@outlook.com>
2023-10-02 17:23:08 +0700

    add add_data route

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,5 +1,6 @@
 // GoRouter configuration
 import 'package:go_router/go_router.dart';
+import 'package:piduiteun/features/add_data/presentation/views/add_data_view.dart';
 import 'package:piduiteun/features/home/presentation/views/bottom_nav_bar_view.dart';
 
 final router = GoRouter(
@@ -7,6 +8,11 @@ final router = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const BottomNavBarView(),
+    ),
+    GoRoute(
+      path: '/add_data',
+      name: 'add_data',
+      builder: (context, state) => const AddDataView(),
     ),
   ],
 );
