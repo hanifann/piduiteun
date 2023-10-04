1a7e8511243ffa932dea7f651c358723bcb07110
hanifan <hanifannurulhaq@outlook.com>
2023-10-04 23:10:09 +0700

    initialize hive

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:piduiteun/app/app.dart';
 import 'package:piduiteun/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+void main() async {
+  await Hive.initFlutter();
+  unawaited(bootstrap(() => const App()));
 }
