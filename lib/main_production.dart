fe7c83e46adcc65171dbf489d1219c81961deac0
hanifan <hanifannurulhaq@outlook.com>
2023-10-05 17:14:42 +0700

    put dependency initializer on last order

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:piduiteun/app/app.dart';
 import 'package:piduiteun/bootstrap.dart';
 import 'package:piduiteun/features/add_data/domain/entities/expenditure.dart';
+import 'package:piduiteun/injection_container.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  Hive.registerAdapter(ExpenditureAdapter());
+  Hive.registerAdapter<Expenditure>(ExpenditureAdapter());
+  await init();
   unawaited(bootstrap(() => const App()));
 }
