6888529d27c77e0fe760d251929d7cead2e5500e
hanifan <hanifannurulhaq@outlook.com>
2023-10-05 17:15:28 +0700

    implement addDataBloc

--- lib/features/add_data/presentation/views/add_data_view.dart
+++ lib/features/add_data/presentation/views/add_data_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
+import 'package:piduiteun/features/add_data/domain/entities/expenditure.dart';
+import 'package:piduiteun/features/add_data/presentation/bloc/add_data_bloc.dart';
 import 'package:piduiteun/features/add_data/presentation/widgets/textfield_with_title_widget.dart';
 import 'package:piduiteun/features/home/domain/entities/category.dart';
 import 'package:piduiteun/features/home/domain/entities/segmented_choice.dart';
 import 'package:piduiteun/features/home/presentation/widgets/category_segmented_btn_widget.dart';
+import 'package:piduiteun/injection_container.dart';
 import 'package:piduiteun/widgets/text_widget.dart';
 
 class AddDataView extends StatelessWidget {
@@ -12,7 +17,10 @@ class AddDataView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AddDataPage();
+    return BlocProvider(
+      create: (context) => sl<AddDataBloc>(),
+      child: const AddDataPage(),
+    );
   }
 }
 
@@ -24,21 +32,24 @@ class AddDataPage extends StatefulWidget {
 }
 
 class _AddDataPageState extends State<AddDataPage> {
-
   SegmentedChoice selectedValue = SegmentedChoice.pengeluaran;
   final keteranganEditingController = TextEditingController();
   final nominalEditingController = TextEditingController();
   final dateEditingController = TextEditingController();
   final timeEditingController = TextEditingController();
 
+  final formKey = GlobalKey<FormState>();
+
+
+  DateTime dateTime = DateTime.now();
   String? chipValue;
 
   @override
   void initState() {
-    dateEditingController.text = 
-      DateFormat('dd MMMM yyyy', 'id').format(DateTime.now());
-    timeEditingController.text = 
-      DateFormat('HH:mm', 'id').format(DateTime.now());
+    dateEditingController.text =
+        DateFormat('dd MMMM yyyy', 'id').format(DateTime.now());
+    timeEditingController.text =
+        DateFormat('HH:mm', 'id').format(DateTime.now());
     super.initState();
   }
 
@@ -58,38 +69,114 @@ class _AddDataPageState extends State<AddDataPage> {
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(16.w, 0, 16.w, 24.h),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: 1.sw,
-              child: categorySegmentedBtnWidget(),
-            ),
-            SizedBox(height: 24.h,),
-            keteranganTextfieldWidget(),
-            SizedBox(height: 16.h,),
-            nominalTextfieldWidget(),
-            SizedBox(height: 16.h,),
-            dateAndTimeRowWidget(context),
-            SizedBox(height: 16.h,),
-            kategoriTextWidget(),
-            categoryWrapWidget(context),
-            const Spacer(),
-            addInExBtnWidget(context),
-          ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 1.sw,
+                child: categorySegmentedBtnWidget(),
+              ),
+              SizedBox(height: 24.h),
+              keteranganTextfieldWidget(),
+              SizedBox(height: 16.h),
+              nominalTextfieldWidget(),
+              SizedBox(height: 16.h),
+              dateAndTimeRowWidget(context),
+              SizedBox(height: 16.h),
+              kategoriTextWidget(),
+              if (selectedValue == SegmentedChoice.pengeluaran)
+                categoryExWrapWidget(context)
+              else
+                categoryInWrapWidget(context),
+              const Spacer(),
+              addDataBlocListener(context),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Wrap categoryWrapWidget(BuildContext context) {
+  BlocListener<AddDataBloc, AddDataState> addDataBlocListener(
+    BuildContext context,
+  ) {
+    return BlocListener<AddDataBloc, AddDataState>(
+      listener: (context, state) {
+        if(state is AddDataSucceed){
+          showDialog<String>(
+            context: context, 
+            builder: (_) {
+              return AlertDialog(
+                title: const Text('Berhasil'),
+                content: const Text('Data pengeluaran berhasil ditambahkan'),
+                actions: [
+                  TextButton(
+                    onPressed: () => context.pop(), 
+                    child: const Text('kembali'),
+                  ),
+                ],
+              );
+            },
+          ).then((_){
+            nominalEditingController.clear();
+            dateEditingController.text =
+                DateFormat('dd MMMM yyyy', 'id').format(DateTime.now());
+            timeEditingController.text =
+                DateFormat('HH:mm', 'id').format(DateTime.now());
+            keteranganEditingController.clear();
+            chipValue = null;
+          });
+        } else if (state is AddDataFailed){
+          showDialog<String>(
+            context: context, 
+            builder: (_) {
+              return AlertDialog.adaptive(
+                title: const Text('Gagal'),
+                content: Text(state.errorMessage),
+                actions: [
+                  TextButton(
+                    onPressed: () => context.pop(), 
+                    child: const Text('kembali'),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      },
+      child: addInExBtnWidget(context),
+    );
+  }
+
+  Wrap categoryInWrapWidget(BuildContext context) {
+    return Wrap(
+      spacing: 8.w,
+      children: [
+        ChoiceChip(
+          selectedColor: Theme.of(context).colorScheme.primaryContainer,
+          backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+          side: BorderSide.none,
+          label: const Text('Salary'),
+          selected: chipValue == 'Salary',
+          onSelected: (value) {
+            setState(() {
+              chipValue = value ? 'Salary' : null;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Wrap categoryExWrapWidget(BuildContext context) {
     return Wrap(
       spacing: 8.w,
       children: ExpanseCategory.values.map((e) {
         return ChoiceChip(
           selectedColor: Theme.of(context).colorScheme.primaryContainer,
-          backgroundColor: Theme.of(context)
-            .colorScheme.secondaryContainer,
+          backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
           side: BorderSide.none,
           label: Text(e.name),
           selected: chipValue == e.name,
@@ -118,7 +205,9 @@ class _AddDataPageState extends State<AddDataPage> {
           flex: 2,
           child: selectDateGestureDetectorWidget(context),
         ),
-        SizedBox(width: 8.w,),
+        SizedBox(
+          width: 8.w,
+        ),
         Expanded(
           child: selectTimeGestureDetectorWidget(context),
         ),
@@ -130,11 +219,11 @@ class _AddDataPageState extends State<AddDataPage> {
     return GestureDetector(
       onTap: () {
         showTimePicker(
-          context: context, 
+          context: context,
           initialTime: TimeOfDay.now(),
           initialEntryMode: TimePickerEntryMode.inputOnly,
         ).then((value) {
-          if(value != null) {
+          if (value != null) {
             setState(() {
               timeEditingController.text = value.format(context);
             });
@@ -149,15 +238,16 @@ class _AddDataPageState extends State<AddDataPage> {
     return GestureDetector(
       onTap: () {
         showDatePicker(
-          context: context, 
-          initialDate: DateTime.now(), 
-          firstDate: DateTime(1900), 
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(1900),
           lastDate: DateTime(2100),
         ).then((value) {
-          if(value != null) {
+          if (value != null) {
             setState(() {
-              dateEditingController.text = 
-                DateFormat('dd MMMM yyyy', 'id').format(value);
+              dateEditingController.text =
+                  DateFormat('dd MMMM yyyy', 'id').format(value);
+              dateTime = value;
             });
           }
         });
@@ -173,7 +263,33 @@ class _AddDataPageState extends State<AddDataPage> {
         height: 48.h,
         child: ElevatedButton(
           onPressed: () {
-            
+            if(formKey.currentState!.validate()){
+              if(selectedValue == SegmentedChoice.pengeluaran){
+                context.read<AddDataBloc>().add(
+                  AddExDataEvent(
+                    expenditure: Expenditure(
+                      expenditure: double.parse(nominalEditingController.text), 
+                      dateTime: dateTime, 
+                      information: keteranganEditingController.text, 
+                      time: timeEditingController.text, 
+                      tag: chipValue!,
+                    ),
+                  ),
+                );
+              } else {
+                context.read<AddDataBloc>().add(
+                  AddInDataEvent(
+                    expenditure: Expenditure(
+                      expenditure: double.parse(nominalEditingController.text), 
+                      dateTime: DateTime.parse(dateEditingController.text), 
+                      information: keteranganEditingController.text, 
+                      time: timeEditingController.text, 
+                      tag: chipValue!,
+                    ),
+                  ),
+                );
+              }
+            }
           },
           style: ElevatedButton.styleFrom(
             shape: RoundedRectangleBorder(
@@ -181,11 +297,11 @@ class _AddDataPageState extends State<AddDataPage> {
             ),
             elevation: 0,
             backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-          ), 
+          ),
           child: CustomTextWidget(
-            text: selectedValue == SegmentedChoice.pengeluaran ? 
-            'Tambah pengeluaran' :
-            'Tambah pemasukan',
+            text: selectedValue == SegmentedChoice.pengeluaran
+                ? 'Tambah pengeluaran'
+                : 'Tambah pemasukan',
             size: 14.sp,
             weight: FontWeight.w500,
             color: Theme.of(context).colorScheme.onPrimaryContainer,
@@ -197,17 +313,17 @@ class _AddDataPageState extends State<AddDataPage> {
 
   TextFieldWithTitleWidget timeTextfieldWidget() {
     return TextFieldWithTitleWidget(
-      title: 'Waktu', 
+      title: 'Waktu',
       textEditingController: timeEditingController,
       errorMessage: 'Waktu todak boleh kosong',
       hint: DateFormat('HH:mm', 'id').format(DateTime.now()),
       isEnabled: false,
     );
   }
-  
+
   TextFieldWithTitleWidget dateTextfieldWidget() {
     return TextFieldWithTitleWidget(
-      title: 'Tanggal', 
+      title: 'Tanggal',
       textEditingController: dateEditingController,
       errorMessage: 'Tanggal todak boleh kosong',
       hint: DateFormat('dd MMMM yyyy', 'id').format(DateTime.now()),
@@ -217,7 +333,7 @@ class _AddDataPageState extends State<AddDataPage> {
 
   TextFieldWithTitleWidget nominalTextfieldWidget() {
     return TextFieldWithTitleWidget(
-      title: 'Nominal', 
+      title: 'Nominal',
       textEditingController: nominalEditingController,
       errorMessage: 'Nominal todak boleh kosong',
       isCurrency: true,
@@ -228,7 +344,7 @@ class _AddDataPageState extends State<AddDataPage> {
 
   TextFieldWithTitleWidget keteranganTextfieldWidget() {
     return TextFieldWithTitleWidget(
-      title: 'Keterangan', 
+      title: 'Keterangan',
       textEditingController: keteranganEditingController,
       errorMessage: 'Keterangan tidak boleh kosong',
       hint: 'Contoh. Ayam geprek',
@@ -237,7 +353,7 @@ class _AddDataPageState extends State<AddDataPage> {
 
   CategorySegmentedBtnWidget categorySegmentedBtnWidget() {
     return CategorySegmentedBtnWidget(
-      selectedValue: <SegmentedChoice>{selectedValue}, 
+      selectedValue: <SegmentedChoice>{selectedValue},
       onSelectionChanged: (value) {
         setState(() {
           selectedValue = value.first;
