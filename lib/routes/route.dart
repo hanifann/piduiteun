bb4a25f3fd33a5d9e9300c873db01debcf91f8cd
hanifan <hanifannurulhaq@outlook.com>
2023-10-05 20:07:55 +0700

    add extra on add_data route

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,6 +1,8 @@
 // GoRouter configuration
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:piduiteun/features/add_data/presentation/views/add_data_view.dart';
+import 'package:piduiteun/features/home/presentation/bloc/home_bloc.dart';
 import 'package:piduiteun/features/home/presentation/views/bottom_nav_bar_view.dart';
 
 final router = GoRouter(
@@ -12,7 +14,13 @@ final router = GoRouter(
     GoRoute(
       path: '/add_data',
       name: 'add_data',
-      builder: (context, state) => const AddDataView(),
+      builder: (context, state) {
+        final extra = state.extra! as HomeBloc;
+        return BlocProvider.value(
+          value: extra,
+          child: const AddDataView(),
+        );
+      },
     ),
   ],
 );
