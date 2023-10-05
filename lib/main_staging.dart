a8d8587ace6a7e89fd872de66130fd35527b8d4f
hanifan <hanifannurulhaq@outlook.com>
2023-10-05 13:46:13 +0700

    register  adapter

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -3,8 +3,10 @@ import 'dart:async';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:piduiteun/app/app.dart';
 import 'package:piduiteun/bootstrap.dart';
+import 'package:piduiteun/features/add_data/domain/entities/expenditure.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(ExpenditureAdapter());
   unawaited(bootstrap(() => const App()));
 }
